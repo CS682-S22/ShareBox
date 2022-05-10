31d770650dfd0364bc8c389eb8e14876267036d7
turutupa <adc.batros@gmail.com>
2022-05-09 20:19:36 -0700

    Added initial project structure

---
+++ bittorrent/src/main/java/App.java
@@ -0,0 +1,9 @@
+/**
+ * @author Alberto Delgado on 5/9/22
+ * @project dsd-final-project-anchitbhatia
+ */
+public class App {
+        public static void main(String[] args){
+            System.out.println("This is bittorrent!");
+        }
+}
