16ad0d207b4f6fbd3ef80d7f6db0ce56d5f162bc
Anchit Bhatia <anchitbhatia23@gmail.com>
2022-05-10 22:09:06 -0700

    Added server properties to Node

--- bittorrent/src/main/java/utils/Node.java
+++ bittorrent/src/main/java/utils/Node.java
@@ -4,11 +4,14 @@ public class Node {
     private final String hostname;
     private final String ip;
     private final int port;
+    private Thread serverThread;
+    private boolean isServerRunning;
 
     public Node(String hostname, String ip, int port) {
         this.hostname = hostname;
         this.ip = ip;
         this.port = port;
+        this.isServerRunning = false;
     }
 
     public String getHostname() {
@@ -21,5 +24,18 @@ public class Node {
 
     public int getPort() {
         return port;
+    }
+
+    protected void initializeServer(Runnable serverObj) {
+        this.serverThread = new Thread (serverObj);
+    }
+
+    public void startServer() {
+        this.isServerRunning = true;
+        this.serverThread.start();
+    }
+
+    public void stopServer() {
+        this.isServerRunning = false;
     }
 }
