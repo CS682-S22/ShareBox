0671c55a81a0f6c9f3d3fe80ff665e944426fd83
Anchit Bhatia <anchitbhatia23@gmail.com>
2022-05-10 21:41:08 -0700

    Added Node class

---
+++ bittorrent/src/main/java/utils/Node.java
@@ -0,0 +1,25 @@
+package utils;
+
+public class Node {
+    private final String hostname;
+    private final String ip;
+    private final int port;
+
+    public Node(String hostname, String ip, int port) {
+        this.hostname = hostname;
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public int getPort() {
+        return port;
+    }
+}
