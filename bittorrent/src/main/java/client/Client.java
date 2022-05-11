884ba842817d935e79e5db6fffcd7674389e7d75
Anchit Bhatia <anchitbhatia23@gmail.com>
2022-05-11 16:56:25 -0700

    Client accepting request from peers

--- bittorrent/src/main/java/client/Client.java
+++ bittorrent/src/main/java/client/Client.java
@@ -1,8 +1,42 @@
 package client;
 
+import utils.Connection;
+import utils.Node;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author Alberto Delgado on 5/9/22
  * @project dsd-final-project-anchitbhatia
  */
-public class Client {
+public class Client extends Node {
+
+    public Client(String hostname, String ip, int port) throws IOException {
+        super(hostname, ip, port);
+        this.initializeServer(new PeerServer());
+    }
+
+    private class PeerServer implements Runnable {
+        private final ExecutorService peerConnectionPool;
+
+        public PeerServer() {
+            this.peerConnectionPool = Executors.newCachedThreadPool();
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (isServerRunning) {
+                    Socket clientSocket = serverSocket.accept();
+                    Connection connection = new Connection(clientSocket);
+                    this.peerConnectionPool.execute(new PeerHandler(connection));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
