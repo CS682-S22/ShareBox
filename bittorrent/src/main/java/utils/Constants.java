b2b7f1ccd2e707576177b46f1d6c11e84d36f3ce
Anchit Bhatia <anchitbhatia23@gmail.com>
2022-05-12 03:56:26 -0700

    Added Constants file

---
+++ bittorrent/src/main/java/utils/Constants.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class Constants {
+    public static enum status {
+        ONLINE ,
+        OFFLINE
+    }
+
+//    public final static String STATUS_ONLINE = "online";
+//    public final static String STATUS_OFFLINE = "offline";
+
+    public final static String KEY_NODE = "key_node";
+    public final static String KEY_STATUS = "key_status";
+}
