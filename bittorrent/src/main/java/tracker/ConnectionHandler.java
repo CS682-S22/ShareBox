76305fb4ea7f1a074ab9e630d2f405936d2c0102
Anchit Bhatia <anchitbhatia23@gmail.com>
2022-05-12 04:08:08 -0700

    Tracker able to serve peer list request

--- bittorrent/src/main/java/tracker/ConnectionHandler.java
+++ bittorrent/src/main/java/tracker/ConnectionHandler.java
@@ -1,37 +1,59 @@
 package tracker;
 
 import com.google.protobuf.InvalidProtocolBufferException;
-import protos.Proto;
+
+import protos.Node.NodeDetails;
 import protos.Proto.Request;
-import protos.Proto.Request.RequestType;
+import protos.Response;
+import protos.Response.FileInfo;
 import utils.Connection;
+import utils.ConnectionException;
+
+import java.util.List;
+import java.util.Map;
 
 public class ConnectionHandler implements Runnable {
     private final Connection connection;
+    private final Tracker tracker;
 
-    public ConnectionHandler(Connection connection) {
+    public ConnectionHandler(Tracker tracker, Connection connection) {
+        this.tracker = tracker;
         this.connection = connection;
+    }
+
+    private void serveRequestPeers(Request request) throws ConnectionException {
+        String fileName = request.getFileName();
+        Map<Long, List<NodeDetails>> fileInfo = this.tracker.getFileInfo(fileName);
+        FileInfo.Builder responseBuilder = FileInfo.newBuilder();
+        if (fileInfo!=null) {
+            for (Map.Entry<Long, List<NodeDetails>> item : fileInfo.entrySet()) {
+                Long pieceNumber = item.getKey();
+                Response.PeersList peersList = Response.PeersList.newBuilder().
+                        addAllNodes(item.getValue()).
+                        build();
+                responseBuilder.putPiecesInfo(pieceNumber, peersList);
+            }
+        }
+        FileInfo response = responseBuilder.build();
+        this.connection.send(response.toByteArray());
     }
 
     @Override
     public void run() {
         while (!this.connection.isClosed()) {
-            byte[] message = connection.receive();
-            if (message == null) return;
-            Proto.Request proto;
-            RequestType requestType;
-            try {
-                proto = Request.parseFrom(message);
-                requestType = proto.getRequestType();
-            } catch (InvalidProtocolBufferException e) {
-                System.out.println("Invalid packet received");
-                e.printStackTrace();
-                return;
-            }
-
-            if (requestType.equals(RequestType.PEER_MEMBERSHIP)) {
-                for (Proto.Torrent torrent : proto.getTorrentsList())
-                    System.out.println(torrent.getFilename());
+            byte[] message = this.connection.receive();
+            if (message!=null) {
+                try {
+                    Request request = Request.parseFrom(message);
+                    if (request.getRequestType().equals(Request.RequestType.REQUEST_PEERS)) {
+                        this.serveRequestPeers(request);
+                    }
+                } catch (InvalidProtocolBufferException e) {
+                    System.out.println("Invalid packet received");
+                    e.printStackTrace();
+                } catch (ConnectionException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
--- bittorrent/src/main/java/utils/Helper.java
+++ bittorrent/src/main/java/utils/Helper.java
@@ -1,9 +1,19 @@
 package utils;
 
+import protos.Node.NodeDetails;
+
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class Helper {
+
+    public static NodeDetails getNodeDetailsObject(Node node) {
+        return NodeDetails.newBuilder().
+                setHostname(node.getHostname()).
+                setPort(node.getPort()).
+                setIp(node.getIp()).
+                build();
+    }
 
     public static String getIpFromSocket(Socket socket) {
         return (((InetSocketAddress) socket.getRemoteSocketAddress())
