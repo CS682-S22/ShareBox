0f627f94ff0213f276fcbe21887ff680ed6b58d5
Anchit Bhatia <anchitbhatia23@gmail.com>
2022-05-12 15:48:31 -0700

    Added method to get piecesInfo from tracker

--- bittorrent/src/main/java/client/Client.java
+++ bittorrent/src/main/java/client/Client.java
@@ -1,11 +1,17 @@
 package client;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import models.Torrent;
+import protos.Proto;
+import protos.Response;
 import utils.Connection;
 import utils.ConnectionException;
+import utils.Helper;
 import utils.Node;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -17,11 +23,45 @@ import static client.ClientInit.initLibrary;
  */
 public class Client extends Node {
     Library library;
+    Connection trackerConnection;
 
     public Client(String hostname, String ip, int port) throws IOException {
         super(hostname, ip, port);
         initializeServer(new PeerServer());
         library = initLibrary();
+        trackerConnection = null;
+    }
+
+    @Override
+    public void startServer() {
+        super.startServer();
+        try {
+            trackerConnection = ClientInit.joinSwarm(hostname, ip, port);
+        } catch (ConnectionException ignored) {
+            // ignore for the time being
+        }
+    }
+
+    private Map<Long, Response.PeersList> getPiecesInformation(String fileName) throws ConnectionException {
+        Proto.Request request = Proto.Request.newBuilder().
+                setNode(Helper.getNodeDetailsObject(this)).
+                setRequestType(Proto.Request.RequestType.REQUEST_PEERS).
+                setFileName(fileName)
+                .build();
+        trackerConnection.send(request.toByteArray());
+        byte[] response = trackerConnection.receive();
+        try {
+            Response.FileInfo fileInfo = Response.FileInfo.parseFrom(response);
+            return fileInfo.getPiecesInfoMap();
+        } catch (InvalidProtocolBufferException e) {
+            return null;
+        }
+    }
+
+    public void downloadFile(Torrent torrent) throws ConnectionException {
+        String fileName = torrent.getName();
+        Map<Long, Response.PeersList> piecesInfo = getPiecesInformation(fileName);
+        System.out.println(piecesInfo);
     }
 
     private class PeerServer implements Runnable {
@@ -33,12 +73,6 @@ public class Client extends Node {
 
         @Override
         public void run() {
-            try {
-                ClientInit.joinSwarm(hostname, ip, port);
-            } catch (ConnectionException ignored) {
-                // ignore for the time being
-            }
-
             try {
                 while (isServerRunning) {
                     Socket clientSocket = serverSocket.accept();
