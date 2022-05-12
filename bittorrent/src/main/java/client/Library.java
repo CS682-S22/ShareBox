2aaf0fcded0fcb738955b6db09e9960afccb624b
turutupa <adc.batros@gmail.com>
2022-05-11 21:37:44 -0700

    something weird with the commit happened

--- bittorrent/src/main/java/client/Library.java
+++ bittorrent/src/main/java/client/Library.java
@@ -3,10 +3,7 @@ package client;
 import models.Torrent;
 import utils.FileIO;
 
-<<<<<<< HEAD
 import java.io.IOException;
-=======
->>>>>>> 8220515fbeb5de19cb94c663c95e84514fdfa3b9
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -14,34 +11,22 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 /**
  * @author Alberto Delgado on 5/11/22
  * @project bittorrent
-<<<<<<< HEAD
  * <p>
  * Same information as torrent file, but additionally it has information
  * on what pieces it has downloaded
  */
 public class Library {
-    Map<String, File> files = new HashMap();
-=======
- */
-public class Library {
-    Map<String, Torrent> files = new HashMap();
->>>>>>> 8220515fbeb5de19cb94c663c95e84514fdfa3b9
+    Map<String, File> files = new HashMap<>();
     ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
     public Library() {
     }
 
     public void add(Torrent torrent) {
-<<<<<<< HEAD
         File file = new File(torrent);
         lock.writeLock().lock();
         try {
             files.put(file.name, file);
-=======
-        lock.writeLock().lock();
-        try {
-            files.put(torrent.name, torrent);
->>>>>>> 8220515fbeb5de19cb94c663c95e84514fdfa3b9
         } finally {
             lock.writeLock().unlock();
         }
@@ -59,21 +44,13 @@ public class Library {
     public void remove(Torrent torrent) {
         lock.writeLock().lock();
         try {
-<<<<<<< HEAD
             files.remove(torrent.name);
-=======
-            files.remove(torrent);
->>>>>>> 8220515fbeb5de19cb94c663c95e84514fdfa3b9
         } finally {
             lock.writeLock().unlock();
         }
     }
 
-<<<<<<< HEAD
     static class File extends Torrent {
-=======
-    private static class File extends Torrent {
->>>>>>> 8220515fbeb5de19cb94c663c95e84514fdfa3b9
         public final boolean[] downloadedPieces;
 
         public File(Torrent t) {
@@ -91,7 +68,6 @@ public class Library {
                     t.announceList,
                     t.infoHash);
 
-<<<<<<< HEAD
             boolean[] p;
             try {
                 p = checkDownloadedPieces();
@@ -115,13 +91,6 @@ public class Library {
             }
 
             return downloadedPieces;
-=======
-            downloadedPieces = checkDownloadedPieces();
-        }
-
-        private boolean[] checkDownloadedPieces() {
-            FileIO.getInstance().read()
->>>>>>> 8220515fbeb5de19cb94c663c95e84514fdfa3b9
         }
     }
 }
