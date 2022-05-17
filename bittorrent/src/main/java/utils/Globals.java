725cb21a67fad1a80260eb4c96240ac7535d19e2
turutupa <adc.batros@gmail.com>
2022-05-17 14:56:33 -0700

    updated ip

--- bittorrent/src/main/java/utils/Globals.java
+++ bittorrent/src/main/java/utils/Globals.java
@@ -8,8 +8,8 @@ package utils;
  * Global modifiable variables
  */
 public class Globals {
-    public static String trackerName = "hostname";
-    public static String trackerIP = "127.0.0.1"; // default ip
+    public static String trackerName = "mcvm001";
+    public static String trackerIP = "mcvm001"; // default ip
     public static int trackerPort = 5000; // default port
     public static Long PIECE_LENGTH_MAX = (long) Math.pow(2, 10);
 }
