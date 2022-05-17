06e7417353989b1846b9a9cbdeeab26a159e30bb
turutupa <adc.batros@gmail.com>
2022-05-16 21:32:01 -0700

    checkpoint

--- bittorrent/src/main/java/utils/Constants.java
+++ bittorrent/src/main/java/utils/Constants.java
@@ -10,4 +10,5 @@ public class Constants {
     public final static String KEY_STATUS = "key_status";
     public final static String TORRENT_EXT = ".torrent";
     public final static long HEARTBEAT_INTERVAL_MS = 5000L;
+    public final static int MAX_CONCURRENT_DOWNLOADS = 10;
 }
